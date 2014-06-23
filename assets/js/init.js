a74a0d107a31f6b9c70bd072f5408a78b59982f8
vagrant <vagrant@shopify>
2014-06-23 21:50:21 +0000

    Initial commit

---
+++ assets/js/init.js
@@ -0,0 +1,14 @@
+var js = {};
+
+(function($) {
+  js = {
+    init: function() {
+      //stuff that needs doc ready
+      $(document).ready(function() {
+
+      });
+    }
+  };
+
+  js.init();
+})(jQuery);
