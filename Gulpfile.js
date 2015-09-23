6435c012056dd805427464c667f573788180f3da
Joe S <joe@pixelbash.co.nz>
2015-09-23 08:39:45 +0000

    Reorganising, separate config file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,94 +1,115 @@
 'use strict';
-var gulp        = require('gulp');
-var less        = require('gulp-less');
-var changed     = require('gulp-changed');
-var watch       = require('gulp-watch');
-var rename      = require('gulp-rename');
-var uglify      = require('gulp-uglify');
-var uglifycss   = require('gulp-uglifycss');
 
-var gulpIgnore  = require('gulp-ignore');
-var gulpShopify = require('gulp-shopify-upload');
- 
+var source     = require('vinyl-source-stream');
+var gulp       = require('gulp');
+var less       = require('gulp-less');
+var changed    = require('gulp-changed');
+var watch      = require('gulp-watch');
+var rename     = require('gulp-rename');
+var uglify     = require('gulp-uglify');
+var uglifycss  = require('gulp-uglifycss');
+var concat     = require('gulp-concat');
+
 var babelify   = require('babelify');
 var uglifyify  = require('uglifyify');
 var browserify = require('browserify');
-var source     = require('vinyl-source-stream');
 
-var mainBowerFiles = require('main-bower-files');
-var concat         = require('gulp-concat');
+var gulpShopify = require('gulp-shopify-upload');
 
 var config = require('./config');
+ 
 
  //Shopify vars
  var shop = {
-    api_key: config.shopify.api_key,
-    password: config.shopify.pass,
-    url: config.shopify.url
+  api_key:  config.shopify.api_key,
+  password: config.shopify.pass,
+  url:      config.shopify.url
  }
 
 gulp.task('js', function () {
   browserify({
-    entries : './assets/js/src/init.js'
+    entries : './_src/assets/js/src/init.js'
   })
   .transform(babelify)
   .transform('debowerify')
   .transform('deamdify')
-  //.transform('uglifyify')
+  .transform('uglifyify')
   .bundle()
   .pipe(source('app.js'))
-  .pipe(gulp.dest('./assets/js/dist')); 
+  .pipe(gulp.dest('./_src/assets/js/dist')); 
 });
 
-gulp.task('js_libs', function() {
-    return gulp.src(mainBowerFiles(), { base: './js/lib' })
-    .pipe(gulpIgnore.include('*.js'))
-    .pipe(concat('plugins.js'))
-    .pipe(uglify())
-    .pipe(rename({dirname: ''}))
-    .pipe(gulp.dest('./assets/js/dist'))
+gulp.task('bower', function() {
+  return gulp.src([
+    './_src/assets/js/lib/jquery/dist/jquery.js',
+    './_src/assets/js/lib/modernizr/modernizr.js'
+  ], { base: './js/lib' })
+  .pipe(concat('plugins.js'))
+  .pipe(uglify())
+  .pipe(rename({dirname: ''}))
+  .pipe(gulp.dest('./_src/assets/js/dist'))
 });
  
 gulp.task('less', function () {
   //Less
-  gulp.src(['./assets/less/style.less','./assets/less/checkout.less'])
+  gulp.src(['./_src/assets/less/style.less'])
   .pipe(less())
   .pipe(uglifycss())
-  .pipe(gulp.dest('./assets/css'));
+  .pipe(gulp.dest('./_src/assets/css'));
+
+  //Shopify now wants scss for it's checkout edit
+  gulp.src(['./_src/assets/less/checkout.less'])
+  .pipe(less())
+  .pipe(uglifycss())
+  .pipe(rename('checkout.scss'))
+  .pipe(gulp.dest('./_src/assets/css'));
 });
 
-gulp.task('changed', function() {
+gulp.task('move_files', function(){
+  //Update assets
   gulp.src([
-    './assets/fnt/**',
-    './assets/img/**',
-    './assets/css/**.css',
-    './assets/js/dist/**.js'])
+    './_src/assets/fnt/**',
+    './_src/assets/img/**',
+    './_src/assets/css/**.css',
+    './_src/assets/css/**.scss',
+    './_src/assets/js/lib/**.js',
+    './_src/assets/js/dist/**.js'])
   .pipe(rename({dirname: ''}))
-  .pipe(changed('./shop/assets'))
-  .pipe(gulp.dest('./shop/assets'))
+  .pipe(changed('./assets'))
+  .pipe(gulp.dest('./assets'))
+  .pipe(gulpShopify(shop.api_key, shop.password, shop.url, shop.theme_id));
 
-  gulp.src('./config/**').pipe(changed('./shop/config')).pipe(gulp.dest('./shop/config'));
-  gulp.src('./layout/**').pipe(changed('./shop/layout')).pipe(gulp.dest('./shop/layout'));
-  gulp.src('./snippets/**').pipe(changed('./shop/snippets')).pipe(gulp.dest('./shop/snippets'));
-  gulp.src('./templates/**').pipe(changed('./shop/templates')).pipe(gulp.dest('./shop/templates'));
+
+  gulp.src('./_src/config/**').pipe(changed('./config')).pipe(gulp.dest('./config')).pipe(gulpShopify(shop.api_key, shop.password, shop.url, shop.theme_id));
+  gulp.src('./_src/layout/**').pipe(changed('./layout')).pipe(gulp.dest('./layout')).pipe(gulpShopify(shop.api_key, shop.password, shop.url, shop.theme_id));
+  gulp.src('./_src/snippets/**').pipe(changed('./snippets')).pipe(gulp.dest('./snippets')).pipe(gulpShopify(shop.api_key, shop.password, shop.url, shop.theme_id));
+  gulp.src('./_src/templates/**').pipe(changed('./templates')).pipe(gulp.dest('./templates')).pipe(gulpShopify(shop.api_key, shop.password, shop.url, shop.theme_id));
+
 });
 
-gulp.task('shopifywatch', function(){
-  return watch(['./shop/'])
-    .pipe(gulpShopify(shop.api_key, shop.password, shop.url,'9083535'));
-})
-
-//Watch
 gulp.task('watch', function(){
-  console.log('started watch')
-  gulp.watch(['./assets/js/src/**'], ['js']);
-  gulp.watch(['./assets/js/lib/**'], ['js_libs']);
-  gulp.watch(['./assets/less/**'], ['less']);
-  gulp.watch(['./+(assets|layout|config|snippets|templates|locales)/**'], ['changed']);
-  gulp.start('shopifywatch');
+  console.log('started watch');
+  gulp.watch(['./_src/assets/js/src/**'], ['js']).on('error', swallowError);
+  gulp.watch(['./_src/assets/js/lib/**'], ['bower']).on('error', swallowError);
+  gulp.watch(['./_src/assets/less/**'],   ['less']).on('error', swallowError);
+
+  gulp.start('move_files');
+  gulp.watch([
+    './_src/assets/**',
+    './_src/config/**',
+    './_src/layout/**',
+    './_src/snippets/**',
+    './_src/templates/**',
+    ], ['move_files'])
+      .on('error', swallowError);
 });  
 
-//Default
-gulp.task('default', function() {
-  gulp.start('js', 'less');
-});
+// Default gulp action when gulp is run
+gulp.task('default', [
+   'watch'
+]);
+
+function swallowError (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
